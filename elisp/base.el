9eb573d8acd85989e03b5aa71d8b194e5dc86b47
Shad Gregory <shad@delllaptop>
2017-07-18 06:42:45 -0500

    add magit

--- elisp/base.el
+++ elisp/base.el
@@ -1,7 +1,12 @@
+(require 'package)
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
 (show-paren-mode 1)
 (fset 'yes-or-no-p 'y-or-n-p)
+(setq visible-bell 1)
 (setq column-number-mode t)
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
@@ -32,5 +37,7 @@
   :config
   (dashboard-setup-startup-hook))
 
+(use-package magit
+  :ensure t)
 
 (provide 'base)
