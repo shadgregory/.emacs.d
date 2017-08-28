5c0c2d1d6662ef1f8384f820349d7226f656cf38
Shad Gregory <shad.gregory@austintexas.gov>
2017-08-28 10:45:02 -0500

    use-package for racket

--- elisp/lispen.el
+++ elisp/lispen.el
@@ -4,20 +4,24 @@
   (progn
     (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
 
+(use-package racket-mode
+  :ensure t
+  :config (add-hook 'racket-mode-hook #'aggressive-indent-mode))
+
 (use-package paredit
   :ensure t
   :init
   (progn
     (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+    (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
     (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
     (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
     (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
     (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
     (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
     (add-hook 'racket-mode-hook           #'enable-paredit-mode)
-    (add-hook 'racket-repl-mode-hook      #'enable-paredit-mode)
-    (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
-    (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)))
+    (add-hook 'racket-repl-mode-hook      #'enable-paredit-mode)))
 
 (use-package clojure-mode
   :ensure t
