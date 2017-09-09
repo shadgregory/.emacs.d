76394b4e77a6dcba4198fcaddb4195ffdecbd8f8
Shad Gregory <Shad.Gregory@austintexas.gov>
2017-09-09 14:33:48 -0500

    rearrange

--- elisp/lispen.el
+++ elisp/lispen.el
@@ -10,21 +10,6 @@
   :ensure t
   :config (add-hook 'racket-mode-hook #'aggressive-indent-mode))
 
-(use-package paredit
-  :ensure t
-  :init
-  (progn
-    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-    (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
-    (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
-    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-    (add-hook 'racket-mode-hook           #'enable-paredit-mode)
-    (add-hook 'racket-repl-mode-hook      #'enable-paredit-mode)))
-
 (use-package clojure-mode
   :ensure t
   :config (add-hook 'clojure-mode-hook #'aggressive-indent-mode))
@@ -42,5 +27,20 @@
   (require 'cmuscheme)
   (let ((scheme-program-name "/usr/local/bin/kawa"))
     (run-scheme scheme-program-name)))
+
+(use-package paredit
+  :ensure t
+  :init
+  (progn
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+    (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+    (add-hook 'racket-mode-hook           #'enable-paredit-mode)
+    (add-hook 'racket-repl-mode-hook      #'enable-paredit-mode)))
 
 (provide 'lispen)
